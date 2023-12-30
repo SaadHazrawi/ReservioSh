0b954c23a273be6d686a60ec6eb3a342f285fd20
Abdullah Jebiro <abdullahjbero@gmail.com>
2023-12-31 01:50:24 +0300

    Update Patient class with validation attributes

--- Hosptil/Models/Patient.cs
+++ Hosptil/Models/Patient.cs
@@ -5,19 +5,23 @@ namespace Hosptil.Models
 {
     public class Patient
     {
-        [Key]
         public int PatientId { get; set; }
-        [StringLength(maximumLength:15,MinimumLength =2)]
-        public string FirstName { get; set; }
-        [StringLength(maximumLength: 15, MinimumLength = 2)]
-        public string LastName { get; set; }
-        [MaxLength(50)]
-        public string Resgoin { get; set; }
+
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 15 characters.")]
+        public string FirstName { get; set; } = null!;
+
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 15 characters.")]
+        public string LastName { get; set; } = null!;
+
+        [MaxLength(50, ErrorMessage = "Resgoin must not exceed 50 characters.")]
+        public string Resgoin { get; set; } = string.Empty;
+
         public GenderPaintet Gender { get; set; }
-        public List<Reservation> Reservations { get; set; }=new List<Reservation>();
+
+        [Range(typeof(DateTime), "1/1/1823", "{0}", ErrorMessage = "Date of birth must be after 1/1/1823.")]
+        public DateTime dateOfBirth { get; set; }
+
+        public string IPAddress { get; set; } = null!;
         public bool IsDeleted { get; set; } = false;
-
-
-
     }
 }
