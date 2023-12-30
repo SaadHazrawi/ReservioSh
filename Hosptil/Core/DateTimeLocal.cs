766d4d03edd5641714e9e0f3a81e1b8f54df503b
Abdullah Jebiro <abdullahjbero@gmail.com>
2023-12-31 02:15:52 +0300

    Add Class DateTimeLocal

---
+++ Hosptil/Core/DateTimeLocal.cs
@@ -0,0 +1,16 @@
+﻿namespace Reservio.Core
+{
+    public static class DateTimeLocal
+    {
+        public static DateTime GetDateTime()
+        {
+            return DateTime.UtcNow.AddHours(3);
+        }
+
+        //TODO I forgot why I did it 😁
+        public static DateTime GetDate()
+        {
+            return DateTime.UtcNow.AddHours(3).Date;
+        }
+    }
+}
