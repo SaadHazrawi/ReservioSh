d99bdad506c3f58d1ad9e11464de8ecb0d695e20
Abdullah Jebiro <abdullahjbero@gmail.com>
2023-12-31 02:14:46 +0300

    Add Date property instead of BookFor

--- Hosptil/Models/Reservation.cs
+++ Hosptil/Models/Reservation.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Hosptil.Models
+namespace Hosptil.Models;
+public class Reservation
 {
-    public class Reservation
-    {
-        public int ReservationId { get; set; }
-        public int PatientId { get; set; }
-        public int ClinicId { get; set; }     
-        public DateTime BookFor { get; set; }
-        public bool IsDeleted { get; set; } = false;
+    public int ReservationId { get; set; }
+    public int PatientId { get; set; }
+    public int ClinicId { get; set; }
+    public string IPAddress { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public DateTime BookFor { get; set; }
+    public bool IsDeleted { get; set; } = false;
 
-        public Clinic Clinic { get; set; } = null!;
-        public Patient Patient { get; set; } = null!;
-
-
-    }
+    public Clinic Clinic { get; set; } = null!;
+    public Patient Patient { get; set; } = null!;
 }
+
