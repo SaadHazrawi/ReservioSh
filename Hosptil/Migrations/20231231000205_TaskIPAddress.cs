fe71d02a950c86566904762d656771046e9e006c
Abdullah Jebiro <abdullahjbero@gmail.com>
2023-12-31 03:03:10 +0300

    Add Migration

---
+++ Hosptil/Migrations/20231231000205_TaskIPAddress.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservio.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskIPAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Reservations",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BookFor",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "IPAddress",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IPAddress",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateOfBirth",
+                table: "Patients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookFor",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "IPAddress",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "IPAddress",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "dateOfBirth",
+                table: "Patients");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Reservations",
+                newName: "DateTime");
+        }
+    }
+}
