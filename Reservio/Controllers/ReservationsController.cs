43be521f59a75ff4338c36eefdbab4919244c9dc
Abdullah Jebiro <abdullahjbero@gmail.com>
2023-12-31 14:07:18 +0300

    Add ReservationsController

--- Reservio/Controllers/ReservationsController.cs
+++ Reservio/Controllers/ReservationsController.cs
@@ -2,10 +2,11 @@
 using Reservio.Services.ReservationRepo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Reservio.DTOS.Reservation;
 
 namespace Reservio.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Reservations")]
     [ApiController]
     public class ReservationsController : ControllerBase
     {
@@ -17,6 +18,66 @@ namespace Reservio.Controllers
             this._reservation = reservation;
             this._mapper = mapper;
         }
+        [HttpGet]
+        public async Task<ActionResult> CheckReservationStatus([FromQuery] string iPAddress)
+        {
+            var reservationStatus = await _reservation.CheckReservationStatus(iPAddress);
+            return Ok(reservationStatus);
 
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Add(ReservationForAddDto dto)
+        {
+            await _reservation.AddReservationAsync(dto);
+            return Ok();
+        }
+
+
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteReservation(string iPAddress)
+        {
+
+            //TODO What is Logic , Issues 1
+            //await _reservation.DeleteReservationAsync(iPAddress);
+            return NoContent();
+        }
+
+
+        [HttpGet("Clinics")]
+        public async Task<ActionResult> GetClinics()
+        {
+            //TODO 
+            //var Clinics = await _reservation.GetClinicsForReservationAsync();
+            //if (Clinics == null)
+            //{
+            //    return NotFound();
+            //}
+            //return Ok(Clinics);
+
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        /// Retrieves the patients present in the clinic on a day.
+        /// </summary>
+        /// <param name="clinicsId">The ID of the clinic.</param>
+        /// <returns>The list of patients in the clinic.</returns>
+
+        [HttpGet("Clinics/{clinicsId}")]
+        public async Task<ActionResult> GetClinics(int clinicsId)
+        {
+
+            var Clinics = await _reservation.GetPatientsInClinic(clinicsId);
+            if (Clinics == null)
+            {
+                return NotFound();
+            }
+            return Ok(Clinics);
+        }
     }
 }
