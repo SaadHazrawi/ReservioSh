4b7a58ea881d86e34ecc087085573f43cb44af89
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-01-01 08:53:55 +0300

    Add Base Repository

---
+++ Reservio/Services/BaseRepo/BaseRepository.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reservio.AppDataContext;
+using Reservio.Core;
+using System.Linq.Expressions;
+
+namespace Reservio.Services.BaseRepo
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+    where T : class
+    {
+        private readonly DataContext _context;
+
+        public BaseRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+
+            query = query.Where(criteria);
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(criteria);
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(
+            Expression<Func<T, bool>> criteria,
+            int? take,
+            int? skip,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.Where(criteria).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(
+            Expression<Func<T, bool>> criteria,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.Where(criteria).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(
+            int? skip,
+            int? take,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            // Order the query by the specified property and direction
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            // Apply the skip and take options
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            // Include related entities if specified
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            // Execute the query and return the results as an IEnumerable
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            _context.SaveChanges();
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+
+
+
+        public async Task DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRangeAsync(Expression<Func<T, bool>> criteria)
+        {
+            var entities = _context.Set<T>().Where(criteria);
+            _context.Set<T>().RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+
+   
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<T>().CountAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> criteria)
+        {
+            return await _context.Set<T>().CountAsync(criteria);
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> criteria)
+        {
+            return await _context.Set<T>().AnyAsync(criteria);
+        }
+
+
+    }
+}
+
---
+++ Reservio/Services/BaseRepo/IBaseRepository.cs
@@ -0,0 +1,93 @@
+﻿using Reservio.Core;
+using System.Linq.Expressions;
+
+namespace Reservio.Services.BaseRepo
+{
+    public interface IBaseRepository<T>
+        where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Gets all entities in the repository, optionally ordering them by the specified criteria.
+        /// </summary>
+        /// <param name="orderBy">A lambda expression specifying the property to order by.</param>
+        /// <param name="orderByDirection">The direction to order the results in.</param>
+        /// <returns>A Task representing the asynchronous operation that returns all entities in the repository.</returns>
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        );
+
+        /// <summary>
+        /// Finds a single entity based on the provided criteria and includes related entities as specified.
+        /// </summary>
+        /// <param name="criteria">The criteria used to filter the entity.</param>
+        /// <param name="includes">The related entities to include in the result set.</param>
+        /// <returns>The entity that satisfies the provided criteria or null if no entity is found.</returns>
+        Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
+
+
+
+        /// <summary>
+        /// Retrieves all entities that match the specified criteria, with optional sorting and pagination.
+        /// </summary>
+        /// <param name="criteria">The criteria to use for filtering the entities.</param>
+        /// <param name="skip">The number of entities to skip. Null to skip no entities.</param>
+        /// <param name="take">The maximum number of entities to retrieve. Null to retrieve all matching entities.</param>
+        /// <param name="includes">The related entities to include in the query results. Null to exclude related entities.</param>
+        /// <param name="orderBy">The property to use for sorting the entities. Null to skip sorting.</param>
+        /// <param name="orderByDirection">The direction to sort the entities in. Defaults to ascending.</param>
+        /// <returns>A collection of entities that match the specified criteria, sorted and paginated if requested.</returns>
+        Task<IEnumerable<T>> FindAllAsync(
+            Expression<Func<T, bool>> criteria,
+            int? skip,
+            int? take,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        );
+
+        /// <summary>
+        /// Retrieves all entities that match the specified criteria, with optional sorting and pagination.
+        /// </summary>
+        /// <param name="criteria">The criteria to use for filtering the entities.</param>
+        /// <param name="includes">The related entities to include in the query results. Null to exclude related entities.</param>
+        /// <param name="orderBy">The property to use for sorting the entities. Null to skip sorting.</param>
+        /// <param name="orderByDirection">The direction to sort the entities in. Defaults to ascending.</param>
+        /// <returns>A collection of entities that match the specified criteria, sorted and paginated if requested.</returns>
+        Task<IEnumerable<T>> FindAllAsync(
+            Expression<Func<T, bool>> criteria,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        );
+
+        /// <summary>
+        /// Retrieves all entities, with optional sorting and pagination.
+        /// </summary>
+        /// <param name="skip">The number of entities to skip. Null to skip no entities.</param>
+        /// <param name="take">The maximum number of entities to retrieve. Null to retrieve all entities.</param>
+        /// <param name="includes">The related entities to include in the query results. Null to exclude related entities.</param>
+        /// <param name="orderBy">The property to use for sorting the entities. Null to skip sorting.</param>
+        /// <param name="orderByDirection">The direction to sort the entities in. Defaults to ascending.</param>
+        /// <returns>A collection of entities, sorted and paginated if requested.</returns>
+        Task<IEnumerable<T>> FindAllAsync(
+            int? skip,
+            int? take,
+            string[] includes = null,
+            Expression<Func<T, object>> orderBy = null,
+            string orderByDirection = OrderBy.Ascending
+        );
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entities);
+        Task DeleteRangeAsync(IEnumerable<T> entities);
+        Task DeleteRangeAsync(Expression<Func<T, bool>> criteria);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> criteria);
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> criteria);
+    }
+}
+
