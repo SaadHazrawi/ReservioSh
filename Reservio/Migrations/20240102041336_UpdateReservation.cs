d879b5dd4b940e6567e0fd854b52126d5f4514c7
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-01-02 07:14:45 +0300

    UpdateReservation

---
+++ Reservio/Migrations/20240102041336_UpdateReservation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservio.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Reservations",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Reservations",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Reservations",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resgoin",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Resgoin",
+                table: "Reservations");
+        }
+    }
+}
--- Reservio/Models/Reservation.cs
+++ Reservio/Models/Reservation.cs
@@ -1,15 +1,38 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Reservio.Core;
+using Reservio.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Reservio.Models;
 public class Reservation
 {
+
     public int ReservationId { get; set; }
-    public int ClinicId { get; set; }
+    [StringLength(50, MinimumLength = 2)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50, MinimumLength = 2)]
+    public string LastName { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    public GenderPaintet Gender { get; set; }
+    public string Resgoin { get; set; } = string.Empty;
+    [StringLength(100, MinimumLength = 2)]
+    public string PhoneNumber { get; set; } = null!;
     public string IPAddress { get; set; } = null!;
     public DateTime Date { get; set; }
     public DateTime BookFor { get; set; }
     public bool IsDeleted { get; set; } = false;
 
+    public int ClinicId { get; set; }
+  
     public Clinic Clinic { get; set; } = null!;
+
+
+   
+
+
+  
+
+ 
+    
 }
 
