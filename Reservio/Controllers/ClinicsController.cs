020b9a481a0f6ce6097fb2b5c2d33cefb4d014eb
saadhzw <saadhzw@gmail.com>
2024-01-03 23:29:22 +0300

    Rename Some DTO ANd Work up in Status Delete Doctor And Replace Schedules Doctor

--- Reservio/Controllers/ClinicsController.cs
+++ Reservio/Controllers/ClinicsController.cs
@@ -10,7 +10,6 @@ namespace Reservio.Controllers
     [ApiController]
     public class ClinicsController : ControllerBase
     {
-        //I stopped the handle error while working and took it to services
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
 
@@ -26,8 +25,7 @@ namespace Reservio.Controllers
         public async Task<IActionResult> GetAllClinics()
         {
             List<Clinic> clinics = await _unitOfWork.Clinics.GetAllCinicsAsync();
-            //if (clinics is null)
-            //    return NotFound("The Clinic Is Empty");
+
             return Ok(_mapper.Map<List<ClinicWithiutAnyThinkAsync>>(clinics));
         }
 
@@ -36,8 +34,6 @@ namespace Reservio.Controllers
         public async Task<IActionResult> CreationClicnic(ClinicCreationDTO clinicWith)
         {
 
-            //if (clinicWith is null)
-            //    return BadRequest();
             var result = await _unitOfWork.Clinics.AddClinicAsync(_mapper.Map<Clinic>(clinicWith));
             return CreatedAtRoute("GetClinic", new { clinicId = result.ClinicId }, _mapper.Map<ClinicWithiutAnyThinkAsync>(result));
         }
@@ -46,8 +42,7 @@ namespace Reservio.Controllers
         public async Task<ActionResult> GetClinic(int clinicId)
         {
             var clinic = await _unitOfWork.Clinics.GetClinicByIdAsync(clinicId);
-            //if (clinic == null)
-            //    return NotFound("This Clinic Is not Found");
+
             return Ok(_mapper.Map<ClinicWithiutAnyThinkAsync>(clinic));
         }
         [HttpPut("{clinicId}")]
@@ -56,8 +51,7 @@ namespace Reservio.Controllers
             if (clinic is null)
                 return BadRequest();
             Clinic clinic1 = await _unitOfWork.Clinics.GetClinicByIdAsync(clinicId);
-            //if (clinic1 is null)
-            //    return NotFound("The Clinic Is not found");
+
             clinic1.Name = clinic.Name;
             await _unitOfWork.Clinics.UpdateClinicAsync(clinic1);
 
@@ -67,14 +61,9 @@ namespace Reservio.Controllers
         public async Task<IActionResult> DeleteClinic(int clinicId)
         {
             Clinic clinic = await _unitOfWork.Clinics.GetClinicByIdAsync(clinicId);
-            //if (clinic is null)
-            //    return NotFound();
             await _unitOfWork.Clinics.DeleteClinicAsync(clinic);
             return NoContent();
         }
-
-        //TODO: I am Abdullah => This idea is not good ^_^ , Why
-        //TODO: Saad=> Because it is like creating a new clinic and not Updating it
         [HttpPost("{clinicId}")]
         public async Task<IActionResult> ActiviteClinic( int clinicId)
         {
--- Reservio/Controllers/DoctorcsController.cs
+++ Reservio/Controllers/DoctorcsController.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Reservio.AppDataContext;
 using Reservio.DTOS.Doctor;
-using Reservio.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using Reservio.Services.DotorRepo;
 using Reservio.Services.BaseRepo;
 
 namespace Reservio.Controllers
@@ -26,9 +22,9 @@ namespace Reservio.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult>GetAllDoctors()
+        public async Task<IActionResult> GetAllDoctors()
         {
-            var doctors=await _unitOfWork.Doctors.GetAllDoctorsAsync();
+            var doctors = await _unitOfWork.Doctors.GetAllDoctorsAsync();
             return Ok(doctors);
 
         }
@@ -37,43 +33,45 @@ namespace Reservio.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateDoctor(DoctorForAddDto doctor)
         {
-            //if(doctor is null)
-            //    return BadRequest();
-            var result=await _unitOfWork.Doctors.AddDoctorAsync(doctor);
-            return CreatedAtRoute(nameof(GetDoctor), new { doctorId=result.DoctorId, includeSubstie=false }, _mapper.Map<DoctorWithoutSubstitueDTO>(result));
-             
+            var result = await _unitOfWork.Doctors.AddDoctorAsync(doctor);
+            return CreatedAtRoute(nameof(GetDoctor), new { doctorId = result.DoctorId, includeSubstie = false }, _mapper.Map<DoctorDTO>(result));
+
         }
 
 
-        [HttpGet("{doctorId}/{includeSubstie}",Name = "GetDoctor")]
-        public async Task<IActionResult> GetDoctor(int doctorId,bool includeSubstie)
+        [HttpGet("{doctorId}/{includeSubstie}", Name = "GetDoctor")]
+        public async Task<IActionResult> GetDoctor(int doctorId, bool includeSubstie)
         {
-            var doctor=await _unitOfWork.Doctors.GetDoctorByIdAsync(doctorId, includeSubstie);
-            //if(doctor is null)
-            //    return NotFound();
-            //if(includeSubstie)
-            //    return Ok(doctor);
-            //else
-            return Ok(_mapper.Map<List<DoctorWithoutSubstitueDTO>>(doctor));
+            var doctor = await _unitOfWork.Doctors.GetDoctorByIdAsync(doctorId, includeSubstie);
+            return Ok(_mapper.Map<List<DoctorDTO>>(doctor));
         }
 
 
         [HttpDelete("{doctorId}")]
         public async Task<IActionResult> DeleteDoctor(int doctorId)
         {
-            //Doctor ? doctor=await _unitOfWork.Doctors.GetDoctorByIdAsync(doctorId,false);
-            //if(doctor is null)
-            //    return NotFound();
+
             await _unitOfWork.Doctors.DeleteDoctorAsync(doctorId);
             return NoContent();
 
         }
 
-
         [HttpPut("{doctorId}")]
         public async Task<IActionResult> UpdateDoctor(int doctorId, DoctorForAddDto dto)
         {
-            await _unitOfWork.Doctors.UpdateDoctorAsync(doctorId , dto);          
+            await _unitOfWork.Doctors.UpdateDoctorAsync(doctorId, dto);
+            return NoContent();
+        }
+        [HttpPut("{scheduleId}/{newDoctorId}")]
+        public async Task<IActionResult> ReplaceScheduleDoctor(int scheduleId, int newDoctorId)
+        {
+            await _unitOfWork.Doctors.ReplaceScheduleDoctorAsync(scheduleId, newDoctorId);
+            return NoContent();
+        }
+        [HttpPut("{oldDoctorId}/{newDoctorId}")]
+        public async Task<IActionResult> ReplaceAllSchedulesDoctor(int oldDoctorId, int newDoctorId)
+        {
+            await _unitOfWork.Doctors.ReplaceAllSchedulesDoctorAsync(oldDoctorId, newDoctorId);
             return NoContent();
         }
     }
