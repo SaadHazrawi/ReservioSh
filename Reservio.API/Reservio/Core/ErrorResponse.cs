d497518073136232fa6178e515a9860f5b732f8a
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-01-18 22:05:58 +0300

    Add class ErrorResponse

---
+++ Reservio.API/Reservio/Core/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Reservio.Core
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse(string message = null)
+        {
+            Message = message;
+        }
+        public string Message { get; set; }
+    }
+}
