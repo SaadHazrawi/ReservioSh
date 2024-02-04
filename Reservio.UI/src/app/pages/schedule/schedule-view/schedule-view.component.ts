22bf2a7205d91c845f1b44b64793dc286e5b6dff
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-02-04 15:15:57 +0300

    fix errors

--- Reservio.UI/src/app/pages/schedule/schedule-view/schedule-view.component.ts
+++ Reservio.UI/src/app/pages/schedule/schedule-view/schedule-view.component.ts
@@ -6,7 +6,7 @@ import { Schedule } from '../Model/Schedule';
 @Component({
   selector: 'ngx-schedule-view',
   templateUrl: './schedule-view.component.html',
-  styleUrls: ['./schedule-view.component.scss']
+  styleUrls: ['./schedule-view.component.css']
 })
 export class ScheduleViewComponent implements OnInit {
 
