e74a38fb1b718067b00fbd870a31cfdc7e4215bb
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-02-04 17:37:57 +0300

    .

--- Reservio.UI/src/app/home/home.module.ts
+++ Reservio.UI/src/app/home/home.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
-import { ThemeModule } from '../@theme/theme.module';
-import { HomeComponent } from './home.component';
-import { NbMenuModule, NbSidebarModule, NbSpinnerModule, NbThemeModule } from '@nebular/theme';
-import { DashboardModule } from '../pages/dashboard/dashboard.module';
-import { ECommerceModule } from '../pages/e-commerce/e-commerce.module';
-import { MiscellaneousModule } from '../pages/miscellaneous/miscellaneous.module';
+import { NbThemeModule } from '@nebular/theme';
+import { HomeComponent } from './home/home.component';
+
 
 
 
 @NgModule({
   declarations: [
-    MainComponent,
     HomeComponent,
   ],
   imports: [
     CommonModule,
     NbThemeModule.forRoot({ name: 'cosmic' }),
     RouterModule.forChild([
-      { path: '', component: MainComponent },
+      { path: '', component: HomeComponent },
       { path: '**', redirectTo: '', pathMatch: 'full' }
     ])
   ]
