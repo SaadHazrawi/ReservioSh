bc96b3b2a353be04fd1e6015ebdbda27bb936996
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-02-04 17:13:57 +0300

    .

--- Reservio.UI/src/app/home/main/main.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
-})
-export class MainComponent  {}
