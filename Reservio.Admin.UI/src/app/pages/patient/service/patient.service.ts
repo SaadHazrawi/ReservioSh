28f055fe69bb10f36b2b1bec2f9a3a03950da37c
Abdullah Jebiro <abdullahjbero@gmail.com>
2024-02-23 03:11:49 +0300

    Update getPatients method to handle pagination and filtering, add addPatient and deletePatient methods

--- Reservio.Admin.UI/src/app/pages/patient/service/patient.service.ts
+++ Reservio.Admin.UI/src/app/pages/patient/service/patient.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { PatientFilter } from '../Model/PatientFilter';
+import { PatientFilter } from '../Model/patientFilter';
+import { catchError, tap } from 'rxjs/operators';
+import { PatientDto } from '../Model/patientDto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +13,53 @@ export class PatientService {
   apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
-  
-  getPatients(patientFilter: PatientFilter): Observable<HttpResponse<any[]>> {
-    const url = `${this.apiUrl}Patients`;
-    var params = {...patientFilter};
-    return this.http.get<any[]>(url, { observe: 'response', params });
+
+  getPatients(patientFilter: PatientFilter): Observable<HttpResponse<PatientDto[]>> {
+    let url = `${this.apiUrl}Patients`;
+
+    // Constructing query parameters
+    let queryParams = `?pageNumber=${patientFilter.pageNumber}&pageSize=${patientFilter.pageSize}`;
+
+    if (patientFilter.firstName) {
+      queryParams += `&firstName=${patientFilter.firstName}`;
+    }
+    if (patientFilter.lastName) {
+      queryParams += `&lastName=${patientFilter.lastName}`;
+    }
+    if (patientFilter.region) {
+      queryParams += `&region=${patientFilter.region}`;
+    }
+    if (patientFilter.gender !== undefined) {
+      queryParams += `&gender=${patientFilter.gender}`;
+    }
+    if (patientFilter.dateOfBirth) {
+      queryParams += `&dateOfBirth=${patientFilter.dateOfBirth}`;
+    }
+
+    url += queryParams; // Append query parameters to URL
+
+    return this.http.get<PatientDto[]>(url, { observe: 'response' })
+      .pipe(
+        tap(response => {
+          console.log('paginationData : ' + response.headers.get('x-pagination'));
+        }),
+        catchError(this.handleError)
+      );
+  };
+
+  addPatient(data: any): Observable<any> {
+    const url = `${this.apiUrl}Clinics`;
+    return this.http.post<any>(url, data).pipe(
+      catchError(this.handleError)
+    );
   }
-
-
+  deletePatient(patientId: number): Observable<void> {
+    const url = `${this.apiUrl}Patients/${patientId}`;
+    return this.http.delete<void>(url)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
 
   handleError(error: HttpErrorResponse) {
     let errorMessage: string = '';
